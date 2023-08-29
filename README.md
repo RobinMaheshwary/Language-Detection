030736d72f6757e58e7ca30f69e68df439572581
Robin Maheshwary <robinmaheshwari1234567@gmail.com>
2023-08-29 22:16:47 +0530

    v1

---
+++ README.md
@@ -0,0 +1,46 @@
+# Language Prediction
+
+## Requirements
+The project requires the following packages to be installed:
+
+- Pandas
+- NumPy
+- Scikit-learn
+- LangDetect
+- LangId
+- FastText
+- Sentence Transformer
+- TensorFlow
+- Matplotlib
+- Seaborn
+
+To install these packages, you can run the following command:
+
+```bash
+> pip install pandas numpy scikit-learn langdetect langid fasttext sentence-transformers tensorflow matplotlib seaborn
+```
+
+To run the project, you can follow these steps:
+
+- Clone the repository to your local machine:
+```bash
+> git clone https://github.com/jaywyawhare/Language-Prediction.git
+```
+
+- Navigate to the project directory:
+```bash
+> cd Language-Prediction
+```
+
+- Install the required packages:
+
+```bash
+> pip install -r requirements.txt
+```
+
+- To deploy it:
+```bash
+> streamlit run main.py
+```
+
+The script will load the dataset, preprocess the data, train and evaluate the models, and display the results.
---
+++ main.py
@@ -0,0 +1,61 @@
+import nbformat
+import numpy as np
+import streamlit as st
+from keras.models import load_model
+import fasttext
+import fasttext.util
+from fasttext.FastText import _FastText
+from keras.models import load_model
+import streamlit.components.v1 as components
+
+model = load_model('./models/FT_MLP4.h5')
+
+
+dic = {0 : 'Arabic', 1 : 'Danish', 2 : 'Dutch', 3 : 'English', 4 : 'French', 5 : 'German', 6 : 'Greek', 7 : 'Hindi', 8 : 'Italian', 9 : 'Kannada', 10 : 'Malayalam', 11 : 'Portugeese', 12 : 'Russian', 13 : 'Spanish', 14 : 'Sweedish', 15 : 'Tamil', 16 : 'Turkish'} 
+
+ft = _FastText('./models/lid.bin')
+
+def ftEmbedding(text):
+    return ft.get_sentence_vector(text)
+
+
+def preprocess_text(text):
+    text = text.lower()
+    text = text.replace("\n", " ")
+    text = text.replace("\t", " ")
+    text = text.replace("\r", " ")
+    text = text.replace("  ", " ")
+    return text
+
+
+st.sidebar.title("Select an option")
+option = st.sidebar.radio("", ('Language Identification Tool', 't-SNE 2D Plot', 't-SNE 3D Plot'))
+
+if option == 'Language Identification Tool':
+    st.title("Language Identification Tool")
+
+    text = st.text_area("Enter the text to be analysed")
+    if st.button("Predict"):
+        text = preprocess_text(text)
+        if len(text) < 6:
+            st.write("Please enter a long text")
+        else:
+            embedding = ftEmbedding(text)
+            embedding = np.array(embedding)
+            embedding = embedding.reshape(1, 16)
+            prediction = model.predict(embedding)
+            prediction = np.argmax(prediction)
+            st.write("The language is of the text is: ", dic[prediction])
+
+elif option == 't-SNE 3D Plot':
+    st.title("t-SNE 3D Plot")
+    HtmlFile = open("./html/plotly3D.html", 'r', encoding='utf-8')
+    source_code = HtmlFile.read()
+    components.html(source_code, height=600, width=600)
+
+elif option == 't-SNE 2D Plot':
+    st.title("t-SNE 2D Plot")
+    HtmlFile = open("./html/plotly2D.html", 'r', encoding='utf-8')
+    source_code = HtmlFile.read()
+    components.html(source_code, height=600, width=600)
+
